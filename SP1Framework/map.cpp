d20a940cd0714d5fc3199f2aa05e12fd5bbc2beb
unknown <1n506@CL36G110.sit.nyp.edu.sg>
2016-08-30 16:46:42 +0800

    Did the overhaul for the winning screen

--- SP1Framework/map.cpp
+++ SP1Framework/map.cpp
@@ -195,7 +195,7 @@ void renderMap(Console *handle) {
 			char currentChar = g_Map.at(row).at(col);
 
 			// Change '8' to a mazey character
-			if (current_level == LEVEL_MENU || current_level == LEVEL_TITLE || current_level == LEVEL_PAUSE || current_level == LEVEL_OVER || current_level == LEVEL_INSTRUCTION) {
+			if (current_level == LEVEL_MENU || current_level == LEVEL_TITLE || current_level == LEVEL_PAUSE || current_level == LEVEL_OVER || current_level == LEVEL_INSTRUCTION || current_level == LEVEL_WON) {
 				colorToPrint = menuColor;
 			}
 
